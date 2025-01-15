bdf9a09b0a0b2bfcfb4a81a3bed857cb6c3d9e4b
Luis Felipe Salazar Ucros <40307832+luisfucros@users.noreply.github.com>
2025-01-15 01:04:51 +0000

    refactor weather client

--- backend/weather_client.py
+++ backend/weather_client.py
@@ -1,17 +1,35 @@
 import requests
+import logging
 import os
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class WeatherClient:
     def __init__(self):
-        self._secret_api = os.getenv("OPEN_WEATHER_API_KEY")
+        self.api_key = os.getenv("OPEN_WEATHER_API_KEY")
+        if not self.api_key:
+            logging.error("API key for OpenWeather not found in environment variables.")
+            raise ValueError("API key for OpenWeather is required and not set.")
     
     def get_weather(self, city: str):
-        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={self._secret_api}&units=metric"
+        if not city:
+            logging.warning("City parameter is empty.")
+            return {"error": "City parameter is required"}
 
-        payload = {}
-        headers = {}
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={self.api_key}&units=metric"
 
-        response = requests.request("GET", url, headers=headers, data=payload)
+        logging.info(f"Requesting weather data for city: {city}")
+        response = requests.get(url)
+        
+        weather_data = response.json()
 
-        return response.json()
+        if response.status_code != 200:
+            error_message = weather_data.get("message", "Unknown error")
+            logging.error(f"Error from API: {error_message}")
+            return {"error": error_message}
+
+        logging.info(f"Weather data retrieved successfully for {city}.")
+        return weather_data
+
